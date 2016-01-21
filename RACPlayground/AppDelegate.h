6d6eb929842af4674e290c02e0e2f5598d124ac7
chenjunyu <chenjunyu@youmi.net>
2016-01-21 14:53:31 +0800

    Initial Commit

---
+++ RACPlayground/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RACPlayground
+//
+//  Created by Jymn_Chen on 16/1/21.
+//  Copyright © 2016年 com.timedancing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
